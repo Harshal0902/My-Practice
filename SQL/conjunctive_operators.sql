57065c2c70bc7dbdd0721a519134b0f4367d251e
Harshal0902 <harshalraikwar07@gmail.com>
2022-06-10 23:06:57 +0530

    ✨ Add Conjunctive Operators

---
+++ SQL/conjunctive_operators.sql
@@ -0,0 +1,5 @@
+-- AND
+SELECT column1, column2 FROM table_name WHERE [condition1] AND [condition2]...AND [conditionN];
+
+-- OR
+SELECT column1, column2 FROM table_name WHERE [condition1] OR [condition2]...OR [conditionN];
