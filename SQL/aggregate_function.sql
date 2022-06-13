675dfb20cdcf3e85930f30b5600d675058785f14
Harshal0902 <harshalraikwar07@gmail.com>
2022-06-13 23:03:16 +0530

    ✨ Add commands for Aggregate function

---
+++ SQL/aggregate_function.sql
@@ -0,0 +1,20 @@
+-- Count
+SELECT COUNT(column_name) FROM table_name WHERE condition;
+
+-- Sum
+SELECT SUM(column_name) FROM table_name;
+
+-- Avg
+SELECT AVG(column_name) FROM table_name;
+
+-- Max
+SELECT MAX(column_name) FROM table_name;
+
+-- Min
+SELECT MIN(column_name) FROM table_name;
+
+-- First
+SELECT FIRST(column_name) FROM table_name;
+
+-- Last
+SELECT LAST(column_name) FROM table_name;
