8757385ef50a7625b8bee184a463c728e89bd412
Harshal0902 <harshalraikwar07@gmail.com>
2022-07-07 21:44:44 +0530

    ✨ Add SQL commands for Index

---
+++ SQL/index.sql
@@ -0,0 +1,8 @@
+-- Create
+CREATE INDEX index_name ON table_name (column_1, column_2);
+
+-- Drop
+DROP INDEX index_name;
+
+-- Create a unique index
+CREATE UNIQUE INDEX index_name ON table_name (column_1, column_2);
